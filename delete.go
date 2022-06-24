72c4891c0214dde0620384bc7282f335c1d31996
Ni30kr91 <nitish.kumar8092@gmail.com>
2022-06-24 17:45:21 +0530

    completed

---
+++ delete.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func delete_user(userID string) string {
+	message := "User not found"
+	user := User{}
+	if user, ok := Data[userID]; ok {
+		delete(Data, userID)
+		message = "user deleted successfully"
+		fmt.Println(user)
+		return message
+	}
+	fmt.Println(user)
+	return message
+}
